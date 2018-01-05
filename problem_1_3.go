8f3a4caef0b7895a6b1cea477bbeceea69dae2c7
tharding <tharding@timbd.us>
2018-01-05 11:41:25 -0700

    first commit

---
+++ problem_1_3.go
@@ -0,0 +1,19 @@
+// Problem:	Solution 2
+// Find the sum of all the multiples of 3 or 5 below 1000
+package main
+
+import "fmt"
+
+func main() {
+
+	total := sumDivisibleBy(3, 999)
+	total += sumDivisibleBy(5, 999)
+	total -= sumDivisibleBy(15, 999)
+
+	fmt.Println("The sum of all multiples of 3 or 5 below 1000 is", total)
+}
+
+func sumDivisibleBy(n, target int) int {
+	p := target / n
+	return n * (p * (p + 1)) / 2
+}
