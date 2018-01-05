2fc9139a048a04625dd66a5e8f67c341321c660b
tharding <tharding@timbd.us>
2018-01-05 10:45:18 -0700

    first commit

---
+++ problem_1.go
@@ -0,0 +1,17 @@
+// Problem:
+// Find the sum of all the multiples of 3 or 5 below 1000
+package main
+
+import "fmt"
+
+func main() {
+
+	var total int
+
+	for i := 1; i < 1000; i++ {
+		if i%3 == 0 || i%5 == 0 {
+			total += i
+		}
+	}
+	fmt.Println("The sum of all multiples of 3 or 5 below 1000 is", total)
+}
