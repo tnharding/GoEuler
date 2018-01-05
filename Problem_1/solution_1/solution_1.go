dbbeaa840a3bceed763079c09940a21e3ce27788
tharding <tharding@timbd.us>
2018-01-05 14:18:02 -0700

    first commit

--- Problem_1/solution_1/solution_1.go
+++ Problem_1/solution_1/solution_1.go
@@ -6,6 +6,18 @@ import "fmt"
 
 func main() {
 
+	total1 := Solution1()
+	total2 := Solution2()
+	total3 := Solution3()
+
+	fmt.Println("The sum of all multiples of 3 or 5 below 1000.")
+	fmt.Println("Solution 1:", total1)
+	fmt.Println("Solution 2:", total2)
+	fmt.Println("Solution 3:", total3)
+}
+
+//Solution1 sum of all the multiples of 3 or 5 below 1000 using for loop
+func Solution1() int {
 	var total int
 
 	for i := 1; i < 1000; i++ {
@@ -13,5 +25,35 @@ func main() {
 			total += i
 		}
 	}
-	fmt.Println("The sum of all multiples of 3 or 5 below 1000 is", total)
+	return total
+}
+
+//Solution2 sum of all the multiples of 3 or 5 below 1000
+func Solution2() int {
+	total := sumDivisibleBy(3, 999)
+	total += sumDivisibleBy(5, 999)
+	total -= sumDivisibleBy(15, 999)
+	return total
+}
+
+func sumDivisibleBy(n, target int) int {
+	var total int
+
+	for i := n; i <= target; i += n {
+		total += i
+	}
+	return total
+}
+
+//Solution3 sum of all the multiples of 3 or 5 below 1000 using a for loop
+func Solution3() int {
+	total := sumDivisibleBy2(3, 999)
+	total += sumDivisibleBy2(5, 999)
+	total -= sumDivisibleBy2(15, 999)
+	return total
+}
+
+func sumDivisibleBy2(n, target int) int {
+	p := target / n
+	return n * (p * (p + 1)) / 2
 }
